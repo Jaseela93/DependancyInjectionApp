dd68edf2f500911542cc8c411bc9a38d0510d5ff
Jaseela93 <jaseelajamal93@gmail.com>
2023-02-17 23:55:12 +0400

    feb 17

---
+++ DependancyInjection/Logic/DemoLogic.cs
@@ -0,0 +1,14 @@
+﻿namespace DependancyInjection.Logic
+{
+    public class DemoLogic
+    {
+        public int value1 { get; private set; }
+        public int value2 { get; private set; }
+
+        public DemoLogic()
+        {
+            value1 = Random.Shared.Next(minValue:1,maxValue:1001);
+            value2 = Random.Shared.Next(minValue: 1, maxValue: 1001);
+        }
+    }
+}
